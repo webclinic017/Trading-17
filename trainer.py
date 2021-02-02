316c242e6f39440a739433776a92919422ba4a88
Harry Burge <burgeharry@gmail.com>
2021-02-02 19:21:18 +0000

    .

--- trainer.py
+++ trainer.py
@@ -1,16 +1,59 @@
-import importlib.util
-from backtesting import Backtest
-
 from backtesting.test import GOOG
+import random
+import numpy as np
+import tensorflow as tf
 
-def main_AI(Strategy_file, Data_file):
-    # Read the bot stratergy
-    spec = importlib.util.spec_from_file_location('bot', Strategy_file)
-    bot = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(bot)
+def main_AI(AI_type_file, Data_file):
 
-    # Spawn an empty version of that bot
-    bot = bot.Bot()
+    print(GOOG)
 
-    bot.train(GOOG)
+    # Create data set
+    test_size = 1
+    train_size = 5
+
+    testX = []
+    testY = []
+    trainX = []
+    trainY = []
+
+    for i in range(test_size):
+
+        index = random.randint(5, GOOG.shape[0]-1)
+        testX.append(GOOG.iloc[[index-5+j for j in range(5)]])
+        testY.append(GOOG.iloc[[index]]['Close'])
+
+    for i in range(train_size):
+
+        index = random.randint(5, GOOG.shape[0]-1)
+        # index = 5
+        trainX.append(np.array(GOOG.iloc[[index-5+j for j in range(5)]]).flatten())
+        trainY.append(np.array(GOOG.iloc[[index]]['Close']))
+
+    trainX = np.array(trainX)
+
+    model = tf.keras.Sequential()
+    model.add(tf.keras.Input(shape=(25,)))
+    model.add(tf.keras.layers.Dense(100, activation="relu"))
+    model.add(tf.keras.layers.Dense(50, activation="relu"))
+    model.add(tf.keras.layers.Dense(5))
+
+    print(model.summary())
+
+    print(trainX.shape)
+    print(trainX[0].shape)
+
+    print(model(trainX[0].reshape(1, -1)).numpy())
+
+
+
+    # # Compile the model
+    # network.compile(
+    #                 optimizer='rmsprop',
+    #                 loss='binary_crossentropy',
+    #                 metrics=['accuracy']
+    #                 )
+
+    # Train the model
+    # network.fit(trainX, trainY, epochs=100)
+
 
