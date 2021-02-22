6546f34f21877750abdd31f745d8b8ff107490ac
Harry Burge <burgeharry@gmail.com>
2021-02-22 18:24:31 +0000

    .

--- trainer.py
+++ trainer.py
@@ -6,6 +6,7 @@ import pandas as pd
 from backtesting import Strategy
 from backtesting import Backtest
 
+
 def SMA(values, n):
     """
     Return simple moving average of `values`, at
@@ -16,8 +17,13 @@ def SMA(values, n):
 class AIBacktest(Strategy):
 
     n1 = 10
+    pos_open = False
+    trailing_value = 0
+    iteration = 0
+    max_loss = 0.04
+    check_every_n_iteration = 4
 
-    check = 0
+    current_trade_start = None
 
     def init(self):
 
@@ -30,7 +36,7 @@ class AIBacktest(Strategy):
 
         # AI
         self.model = tf.keras.Sequential()
-        self.model.add(tf.keras.Input(shape=(26,)))
+        self.model.add(tf.keras.Input(shape=(25,)))
         self.model.add(tf.keras.layers.Dense(5, activation="relu"))
         self.model.add(tf.keras.layers.Dense(1))
 
@@ -44,24 +50,39 @@ class AIBacktest(Strategy):
 
     def next(self):
 
-        self.check += 1
+        if not(self.pos_open):
 
-        if self.check > 1:
-            self.check = 0
+            if self.iteration > self.check_every_n_iteration:
+                self.iteration = 0
 
-            input_data = np.array((self.data.Open[-5:], self.data.Close[-5:], self.data.High[-5:], self.data.Low[-5:], self.data.Volume[-5:]))
-            central_data = input_data - np.array((self.sma_open[-5:], self.sma_close[-5:], self.sma_high[-5:], self.sma_low[-5:], self.sma_volumne[-5:]))
-            central_data = np.append(central_data.reshape(1, -1), self.position.pl_pct)
+                input_data = np.array((self.data.Open[-5:], self.data.Close[-5:], self.data.High[-5:], self.data.Low[-5:], self.data.Volume[-5:]))
+                central_data = input_data - np.array((self.sma_open[-5:], self.sma_close[-5:], self.sma_high[-5:], self.sma_low[-5:], self.sma_volumne[-5:]))
 
-            out = self.model.predict(central_data.reshape(1, -1))
+                out = self.model.predict(central_data.reshape(1, -1))
 
-            if out > 100:
-                self.buy()
-            elif out < 100:
-                self.position.close()
+                if out > 100:
+                    file = open('AI_Data/5Back_Fitness.csv', 'a')
+                    file.write(','.join(['%.5f' % num for num in central_data.reshape(1, -1)[0]]))
+                    file.close()
+                    self.buy()
+                    self.pos_open = True
+
             else:
-                # Do nothing
-                pass
+                self.iteration += 1
+
+        else:
+
+            if self.data.Close[-1] > self.trailing_value:
+                self.trailing_value = self.data.Close * (1-self.max_loss)
+
+            elif self.data.Close[-1] < self.trailing_value:
+                file = open('AI_Data/5Back_Fitness.csv', 'a')
+                file.write('Comp\n')
+                file.close()
+                self.position.close()
+                self.pos_open = False
+
+
 
 
 def fitness(stats):
@@ -73,7 +94,7 @@ def main_AI(AI_type_file, Data_file):
     
     data = pd.read_csv(Data_file)
 
-    bt = Backtest(GOOG, AIBacktest, cash=10_000, commission=.00)
+    bt = Backtest(GOOG, AIBacktest, cash=10_000, commission=.02)
     stats = bt.run()
 
     print(stats)
